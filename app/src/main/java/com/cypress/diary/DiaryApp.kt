28652de3ae8d7343bbfd505b34fe5d001ee6d277
cypress <204089915@qq.com>
2026-05-24 13:47:24 +0800

    fix: simplify diary app scaffold

--- app/src/main/java/com/cypress/diary/DiaryApp.kt
+++ app/src/main/java/com/cypress/diary/DiaryApp.kt
@@ -1,34 +1,17 @@
 package com.cypress.diary
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.cypress.diary.ui.theme.DiaryTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DiaryApp() {
     DiaryTheme {
         Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(text = "Diary")
-                    }
-                )
-            },
             bottomBar = {
                 // Bottom navigation will be added by the navigation task.
             }
-        ) { innerPadding ->
-            Text(
-                text = "Diary app",
-                modifier = Modifier.padding(innerPadding)
-            )
+        ) {
         }
     }
 }
