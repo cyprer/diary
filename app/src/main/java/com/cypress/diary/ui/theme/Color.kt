727c53801c98b1a1468326fd8fe89782d9ec6f06
cypress <204089915@qq.com>
2026-05-24 13:41:33 +0800

    chore: scaffold Android Compose app

---
+++ app/src/main/java/com/cypress/diary/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.cypress.diary.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DiaryPrimary = Color(0xFF3F6652)
+val DiaryOnPrimary = Color(0xFFFFFFFF)
+val DiaryPrimaryContainer = Color(0xFFC1ECD2)
+val DiaryOnPrimaryContainer = Color(0xFF002114)
+
+val DiarySecondary = Color(0xFF53665A)
+val DiaryOnSecondary = Color(0xFFFFFFFF)
+val DiarySecondaryContainer = Color(0xFFD6E8DA)
+val DiaryOnSecondaryContainer = Color(0xFF111F17)
+
+val DiaryBackground = Color(0xFFFBFDF8)
+val DiaryOnBackground = Color(0xFF191C1A)
+val DiarySurface = Color(0xFFFBFDF8)
+val DiaryOnSurface = Color(0xFF191C1A)
